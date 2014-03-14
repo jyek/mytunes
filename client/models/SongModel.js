0a5b9fcf1fa366a079da4b02f5ef8e521b130793
jyek <justinyek@gmail.com>
2014-03-13 21:40:27 -0700

    Removed unused files

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -1,10 +1,5 @@
 // SongModel.js - Defines a backbone model class for songs.
 var SongModel = Backbone.Model.extend({
-  defaults: {
-    url: '',
-    title: '',
-    artist: ''
-  },
 
   play: function(){
     this.trigger('play', this);
@@ -17,6 +12,10 @@ var SongModel = Backbone.Model.extend({
 
   dequeue: function(){
     this.trigger('dequeue', this);
+  },
+
+  stop: function(){
+    this.trigger('stop', this);
   }
 
 });
